b7f923b2534665c5f1efae9605acd78e44849f68
Brunni132 <f_bron@hotmail.com>
2019-01-04 14:15:06 +0900

    Removed support for 4444 textures

--- vdp/utils.js
+++ vdp/utils.js
@@ -143,43 +143,6 @@ export function loadTexture(gl, url) {
 	});
 }
 
-/**
- * @param gl {WebGLRenderingContext}
- * @param url {string}
- * @param done {function(WebGLTexture, HTMLImageElement)}
- * @returns {WebGLTexture}
- */
-export function loadTexture4444(gl, url, done) {
-	const texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-
-	const level = 0;
-	const internalFormat = gl.RGBA;
-	const width = 1;
-	const height = 1;
-	const border = 0;
-	const srcFormat = gl.RGBA;
-	const srcType = gl.UNSIGNED_SHORT_4_4_4_4;
-	const pixel = new Uint16Array([0xff00]);  // opaque blue
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-		width, height, border, srcFormat, srcType,
-		pixel);
-
-	const image = new Image();
-	image.onload = function() {
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-		if (done) done(texture, image);
-	};
-	image.src = url;
-
-	return texture;
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Buffer - memory
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
