30d6ad812fe57ff865038ffdba3e28cd180f2212
Florian <f_bron@hotmail.com>
2019-01-09 13:53:00 +0900

    Much improved advanced color swap sample

--- samples/advanced-color-swap.js
+++ samples/advanced-color-swap.js
@@ -6,17 +6,27 @@ import {color32} from "./vdp/color32";
 function *main(vdp) {
 	let loop = 0;
 
-	// The colors at (x, y) in build/palettes.png that we want to modify
-	const swaps1 = new LineColorArray(3, 10);
+	// The colors at (x, y) in build/palettes.png that we want to modify.
+	// First is the background of the first section of the 'tmx' map. Second is the red of the 'mario' sprite.
+	const swaps = [new LineColorArray(3, 10), new LineColorArray(1, 1)];
 
-	// Use the two first palettes. One is a gradient black -> white, the other black -> red.
-	const gradientPalette = vdp.readPaletteMemory(0, 0, 16, 1, VDPCopySource.blank);
-	for (let i = 0; i < 16; i++) gradientPalette.buffer[i] = color32.make(i * 16, 0, 0);
-	vdp.writePaletteMemory(0, 0, 16, 1, gradientPalette);
+	// Set up the 2 palettes with a gradient (32 and 33, make them far enough so that they're not used)
+	const gradientPalette = vdp.readPaletteMemory(0, 32, 16, 2, VDPCopySource.blank);
+	for (let i = 0; i < 16; i++) gradientPalette.setElement(i, 0, color32.make(i * 16, 0, 0));
+	for (let i = 0; i < 16; i++) gradientPalette.setElement(i, 1, color32.make(i * 16, i * 16, i * 16));
+	vdp.writePaletteMemory(0, 32, 16, 2, gradientPalette);
+
+	// Note that the color (0, 32) is not used, it's transparent so it will let the backdrop appear
+	vdp.configBDColor('#000');
+
+	// Set up another gradient for Mario's costume. Note that where the color 0 is used, the sprite will be transparent.
+	for (let i = 0; i < swaps[1].length; i++) {
+		swaps[1].setLine(i, 33, Math.abs(i - swaps[1].length / 2) / 8);
+	}
 
 	while (true) {
-		for (let i = 0; i < swaps1.length; i++) {
-			const intensity = 16 * i / swaps1.length;
+		for (let i = 0; i < swaps[0].length; i++) {
+			const intensity = 16 * i / swaps[0].length;
 			const floatingPart = intensity - Math.floor(intensity);
 			const blink = loop % 2 ? 1 : 0;
 			const mesh = i % 2 ? blink : 0;
@@ -25,11 +35,13 @@ function *main(vdp) {
 			else if (floatingPart <= 0.5) targetColorIndex = intensity - blink;
 			else if (floatingPart <= 0.75) targetColorIndex = intensity - mesh;
 			else targetColorIndex = intensity;
-			swaps1.setLine(i, 0, targetColorIndex);
+			swaps[0].setLine(i, 32, targetColorIndex);
 		}
 
-		vdp.drawBG('tmx', { scrollY: loop / 2, scrollX: 300 - loop / 2 });
-		vdp.configColorSwap([swaps1]);
+		vdp.drawBG('tmx', { scrollX: 32, scrollY: 0 });
+		vdp.drawObj(vdp.sprite('mario').tile(0), 96, 96 + Math.sin(loop / 90) * 96, { width: 64, height: 64 });
+
+		vdp.configColorSwap(swaps);
 		loop += 1;
 		yield 0;
 	}
