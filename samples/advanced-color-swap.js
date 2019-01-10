9acb9e94f888c042581eb8cca16b394c1f9a486d
Brunni132 <f_bron@hotmail.com>
2019-01-10 21:24:39 +0900

    Improved color swap example's gradient

--- samples/advanced-color-swap.js
+++ samples/advanced-color-swap.js
@@ -1,6 +1,4 @@
-import {LineColorArray, VDPCopySource} from "./vdp/vdp";
-import {startGame} from "./lib-main";
-import {color32} from "./vdp/color32";
+import {LineColorArray, VDPCopySource, startGame, color32} from "./lib-main";
 
 /** @param vdp {VDP} */
 function *main(vdp) {
@@ -12,12 +10,12 @@ function *main(vdp) {
 
 	// Set up the 2 palettes with a gradient (32 and 33, make them far enough so that they're not used)
 	const gradientPalette = vdp.readPaletteMemory(0, 32, 16, 2, VDPCopySource.blank);
-	for (let i = 0; i < 16; i++) gradientPalette.setElement(i, 0, color32.make(i * 16, 0, 0));
+	for (let i = 0; i < 16; i++) gradientPalette.setElement(i, 0, color32.make(0, i * 16, 128 + i * 8));
 	for (let i = 0; i < 16; i++) gradientPalette.setElement(i, 1, color32.make(i * 16, i * 16, i * 16));
 	vdp.writePaletteMemory(0, 32, 16, 2, gradientPalette);
 
 	// Note that the color (0, 32) is not used, it's transparent so it will let the backdrop appear
-	vdp.configBDColor('#000');
+	vdp.configBDColor('#008');
 
 	// Set up another gradient for Mario's costume. Note that where the color 0 is used, the sprite will be transparent.
 	for (let i = 0; i < swaps[1].length; i++) {
