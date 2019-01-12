67d78664de603bee66498cd3481eed596ec56cfc
Brunni132 <f_bron@hotmail.com>
2019-01-12 11:19:36 +0900

    Improved the structure of the mode 7 sample

--- game-main.js
+++ game-main.js
@@ -1,54 +1,64 @@
-import {color32, LineTransformationArray, LineColorArray, startGame} from "./lib-main";
-import {mat3} from "gl-matrix";
+import {startGame, SCREEN_HEIGHT} from "./lib-main";
 
-function makeRainbowColor(angle) {
-	return color32.hslToRgb({ h: angle, l: 0.5, s: 0.2});
+let mapData;
+
+function mapCell(x, y) {
+	return mapData.getElement(Math.floor(x / 8), Math.floor(y / 8));
 }
 
-/** @param vdp {VDP} */
+/**
+ * @param vdp {VDP}
+ */
 function *main(vdp) {
-	let loop = 0;
-	const mario = { x: 120, y: 128, w: 16, h: 16 };
+	const mario = {
+		x: 100, y: 0, w: 48, h: 48, vx: 0.5, vy: 0
+	};
 
-	const maskBgPalNo = vdp.palette('mask-bg').y;
-	// Replace color 0 (outside of mask) with opaque color 1 (gray), and color 2 (central band) with transparent color 0
-	const colorReplacements = [ new LineColorArray(0, maskBgPalNo), new LineColorArray(2, maskBgPalNo) ];
-	colorReplacements[0].setAll(1, maskBgPalNo);
-	colorReplacements[1].setAll(0, maskBgPalNo);
+	// Take all background colors and make them 0 (transparent). Hack to be able to reuse the map.
+	const bgTileset = vdp.readSprite('level1');
+	bgTileset.buffer.forEach((pixel, index) => {
+		// Pixels are 4 bit per pixel, packed in a 8 bit integer. Therefore there are two inside; we need to extract them the old way
+		let pix1 = pixel & 0xf, pix2 = pixel >> 4;
+		if (pix1 === 1 || pix1 === 11 || pix1 === 12 || pix1 === 15)
+			pix1 = 0;
+		if (pix2 === 1 || pix2 === 11 || pix2 === 12 || pix2 === 15)
+			pix2 = 0;
+
+		// Make the final pixel
+		bgTileset.buffer[index] = pix1 | pix2 << 4;
+	});
+	vdp.writeSprite('level1', bgTileset);
+
+	vdp.configBDColor('#000');
+	mapData = vdp.readMap('level1');
 
 	while (true) {
-		mario.x = 120 + Math.cos(loop / 90) * 64;
-		mario.y = 120 + Math.sin(loop / 90) * 112;
+		mario.x += mario.vx;
+		mario.y += mario.vy;
+		mario.vy += 0.05; // gravity
 
-		const lineTransform = new LineTransformationArray();
-		for (let y = 0; y < lineTransform.length; y++) {
-			const center = { x: Math.floor(mario.x + mario.w / 2), y: Math.floor(mario.y + mario.h / 2) };
-			let scale = 0;
-			const circleRay = Math.max(32, 300 - loop * 3);
-			// Visible on that line?
-			if (Math.abs(y - center.y) < circleRay) {
-				const angle = Math.asin((y - center.y) / circleRay);
-				scale = Math.cos(angle) * circleRay;
-			}
-
-			const t = mat3.create();
-			// Centered on the 4th pixel of the mask-bg horizontally
-			mat3.translate(t, t, [4, 0]);
-			mat3.scale(t, t, [1 / scale, 1 / scale]);
-			// This is a case where we want to use the row 0 all the time (the tilemap is only 8x1 pixels)
-			mat3.translate(t, t, [-center.x, 0]);
-			lineTransform.setLine(y, t);
+		while (mapCell(mario.x + mario.w / 2, mario.y + mario.h) >= 202) {
+			mario.vy = 0;
+			mario.y = Math.floor(mario.y - 1);
 		}
 
-		vdp.configBGTransparency({ op: 'add', blendDst: makeRainbowColor(loop / 200), blendSrc: '#000' });
-		vdp.configColorSwap(colorReplacements);
+		// Fell down too much? Bring back up…
+		if (mario.y > SCREEN_HEIGHT) mario.y = -mario.h;
 
-		vdp.drawBG('level1');
-		vdp.drawBG('mask-bg', { wrap: false, transparent: true, lineTransform });
+		// Camera is 96 pixels behind Mario, but doesn't go beyond 0 (beginning of the map)
+		const cameraX = Math.max(0, mario.x - 96);
 
-		vdp.drawObj(vdp.sprite('mario').tile(0), mario.x, mario.y);
+		// Configure OBJ transparency as shadow
+		vdp.configOBJTransparency({ op: 'add', blendDst: '#888', blendSrc: '#000' });
+		vdp.drawBG('tmx', { scrollX: cameraX / 2, scrollY: -24, wrap: false, prio: 0 });
+		vdp.drawBG('level1', { scrollX: cameraX, wrap: false, prio: 1 });
 
-		loop += 1;
+		// Draw sprite with two shadow sprites, a small one with high prio (for planes with prio=0 and 1), a large one with low prio (for plane with prio=0)
+		const marioTile = vdp.sprite('mario').tile(2);
+		vdp.drawObj(marioTile, mario.x - cameraX, mario.y, { prio: 2, width: mario.w, height: mario.h });
+		vdp.drawObj(marioTile, mario.x - cameraX + 2, mario.y + 2, { prio: 2, width: mario.w, height: mario.h, transparent: true });
+		vdp.drawObj(marioTile, mario.x - cameraX + 5, mario.y + 5, { prio: 1, width: mario.w, height: mario.h, transparent: true });
+
 		yield 0;
 	}
 }
--- samples/mode7.js
+++ samples/mode7.js
@@ -1,7 +1,34 @@
 import {LineTransformationArray, startGame, SCREEN_WIDTH, SCREEN_HEIGHT} from './lib-main';
 import { mat3, vec3 } from 'gl-matrix';
 
-// Just an attempt, doesn't quite work. Use reference instead: https://www.coranac.com/tonc/text/mode7.htm
+// You can play with that for the perspective
+function scaleAtLine(line) { return 100 / (line + 50); }
+
+function drawSprite(vdp, transformations, x, z, obj) {
+	// I'll leave the computation to someone better at math than me ;) it's super inefficient but does the trick.
+	// Here I just try to find the screen-space line on which the sprite is the most fitted (if any) by doing the inverse
+	// transformation on each line; if it's right, the 'y' component of the transformed vector should be around 0.
+	const mat = mat3.create();
+	const untransformed = vec3.fromValues(x, z, 1);
+	const result = vec3.create();
+	let line, scale;
+	for (line = transformations.length - 1; line >= 0; line--) {
+		scale = scaleAtLine(line);
+		mat3.invert(mat, transformations[line]);
+		vec3.transformMat3(result, untransformed, mat);
+		if (Math.abs(result[1] ) < scale * 2) {
+			break;
+		}
+	}
+	if (line <= 0) return;
+
+	// Once found, we transform the object using the scale level at that line.
+	// The current (result[0], result[1]) are the positions of the (center, bottom) anchor of the sprite
+	scale = 1 / scale;
+	vdp.drawObj(obj, result[0] - scale * obj.w / 2, line - scale * obj.h, { width: obj.w * scale, height: obj.h * scale, prio: 2 });
+}
+
+// Just a quick attempt. Use reference instead: https://www.coranac.com/tonc/text/mode7.htm
 function *main(vdp) {
 	const lineTransform = new LineTransformationArray();
 	let loop = 0;
@@ -12,7 +39,7 @@ function *main(vdp) {
 
 		const transformations = [];
 		for (let line = 0; line < SCREEN_HEIGHT + 64; line++) {
-			const scale = 100 / (line + 50);
+			const scale = scaleAtLine(line);
 			const mat = mat3.create();
 			mat3.translate(mat, mat, viewerPos);
 			mat3.rotate(mat, mat, viewerAngle);
@@ -27,31 +54,8 @@ function *main(vdp) {
 		}
 
 		vdp.drawBG('road', { lineTransform, winY: 0, wrap: true});
-
-		// (x, z)
-		function computeSpritePos(spritePos) {
-			// I'll leave the computation to someone better at math than me ;)
-			const mat = mat3.create();
-			const untransformed = vec3.fromValues(spritePos[0], spritePos[1], 1);
-			const result = vec3.create();
-			let line, scale;
-			for (line = transformations.length - 1; line >= 0; line--) {
-				scale = 100 / (line + 50);
-				mat3.invert(mat, transformations[line]);
-				vec3.transformMat3(result, untransformed, mat);
-				if (Math.abs(result[1] ) < scale * 2) {
-					break;
-				}
-			}
-			if (line <= 0) return;
-
-			// The current (result[0], result[1]) are the positions of the (center, bottom) anchor of the sprite
-			const obj = vdp.sprite('mario').tile(6);
-			scale = 1 / scale;
-			vdp.drawObj(obj, result[0] - scale * obj.w / 2, line - scale * obj.h, { width: obj.w * scale, height: obj.h * scale, prio: 2 });
-		}
-
-		computeSpritePos([512, 351]);
+		drawSprite(vdp, transformations, 512, 351, vdp.sprite('mario').tile(6));
+		drawSprite(vdp, transformations, 550, 400, vdp.sprite('mario').tile(2));
 
 		loop += 1;
 		yield 0;
