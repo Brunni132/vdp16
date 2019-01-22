0373e847d7846f9600267da9ae72dcda85785961
Brunni132 <f_bron@hotmail.com>
2019-01-23 04:54:10 +0900

    Slightly more friendly output in packer.ese

--- tools/gfxConverter/executable.js
+++ tools/gfxConverter/executable.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-let code = fs.readFileSync('gfx/packer-main.js', 'utf-8');
-
+const utils = require('./utils');
 const args = process.argv.slice(2);
 let convertOnly = false;
+
 args.forEach((v) => {
 	if (/help/.test(v)) {
 		console.log(`Usage: packer [--convert-only]`);
@@ -11,6 +11,9 @@ args.forEach((v) => {
 	if (/convert-only/.test(v)) convertOnly = true;
 });
 
+console.log('Packing graphics into build directory…');
+
+let code = fs.readFileSync('gfx/packer-main.js', 'utf-8');
 code = code.replace(/import.*?;/g, '');
 code = `(function({conv,currentPalette,currentPaletteMultiple,currentTileset,paletteNamed,spriteNamed,tilesetNamed,mapNamed,addColors,blank,config,image,map,multiPalette,palette,sprite,tileset,tiledMap}){${code}})`;
 eval(code)(require('./dsl'));
@@ -21,4 +24,5 @@ if (!convertOnly) {
 	const port = 8080;
 	app.use(express.static('./'));
 	app.listen(port, () => console.log(`Open your browser to: http://localhost:${port}/`));
+	console.log('After you\'ve made changes to graphics, close and rerun this app'.formatAs(utils.BRIGHT, utils.FG_GREEN));
 }
